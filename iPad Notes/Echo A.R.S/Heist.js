bae748293f7119ac591da00e2afe6b6036fa5d5e
Nova <robloxmuffinzcafe@gmail.com>
2017-10-03 18:27:05 -0400

    Update Heist.js

--- iPad Notes/Echo A.R.S/Heist.js
+++ iPad Notes/Echo A.R.S/Heist.js
@@ -1,1 +1,21 @@
-
+.auto !heist play={init}
+#js >>
+use Bank;
+use Heist;
+var players = Heist["Players"];
+var listplayers = Heist["PlayerIDs"];
+if(Bank[UserID] < 100) {
+  resp = "You must have at least **100 credits**, <@" + UserID + ">";
+} else {
+  if(Heist["in|progress"]) {
+    resp = "A game is already in session, <@" + UserID + ">";
+  } else {
+    if(players) {
+      players++
+      listplayers = listplayers + ", " + UserID;
+      resp = "You have joined the Heist, <@" + UserID + ">.";
+    } else {
+      players = 1;
+      
+      resp = "<@" + UserID + "> is planning a heist. Type `!heist play` to join!";
+      

5694c41dd5fac0f65518a5bf139755428cbd6805
Nova <robloxmuffinzcafe@gmail.com>
2017-10-05 07:10:33 -0400

    Update Heist.js

--- iPad Notes/Echo A.R.S/Heist.js
+++ iPad Notes/Echo A.R.S/Heist.js
@@ -1,28 +1,25 @@
-.auto !heist play={init}
+.auto &{:}!(?i)(heist play|hplay)={init}
 #js >>
 use Bank;
 use Heist;
 var players = Heist["Players"];
 var listplayers = Heist["PlayerIDs"];
-if(Heist["PlayerIDs"].indexOf(UserID)) {
-  resp = "You're already in the Heist, <@" + UserID + ">!";
+if(Bank[UserID] < 100) {
+  resp = "You must have at least **100 credits**, <@" + UserID + ">";
 } else {
-  if(Bank[UserID] < 100) {
-    resp = "You must have at least **100 credits**, <@" + UserID + ">";
-  } else {
-    if(Heist["in|progress"]) {
+  if(Heist["in|progress"]) {
     resp = "A game is already in session, <@" + UserID + ">";
+  } else {
+    if(players) {
+      Heist["Players"]++;
+      Heist["PlayerIDs"] = Heist["PlayerIDs"].slice(0, -1) + ', ' + UserID + '"]';
+      resp = "You have joined the Heist, <@" + UserID + ">.";
     } else {
-      if(players) {
-        Heist["Players"]++;
-        Heist["PlayerIDs"] = Heist["PlayerIDs"].slice(0, -1) + ', ' + UserID + '"]';
-        resp = "You have joined the Heist, <@" + UserID + ">.";
-      } else {
-        Heist["Players"] = 1;
-        Heist["PlayerIDs"] = '["' + UserID + '"]';
-        resp = "<@" + UserID + "> is planning a Heist. Type `!heist play` to join!";
-      }
+      Heist["Players"] = 1;
+      Heist["PlayerIDs"] = '["' + UserID + '"]';
+      resp = "<@" + UserID + "> is planning a Heist. Type `!heist play` to join!";
     }
   }
 }
 >>
+{ars:BeginGame}

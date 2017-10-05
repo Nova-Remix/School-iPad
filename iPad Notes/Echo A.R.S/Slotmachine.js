6183fde4d8a97d97c51f03a5cebd7cd6487a0160
Nova <robloxmuffinzcafe@gmail.com>
2017-10-05 13:52:08 -0400

    Update Slotmachine.js

--- iPad Notes/Echo A.R.S/Slotmachine.js
+++ iPad Notes/Echo A.R.S/Slotmachine.js
@@ -2,13 +2,13 @@
 #js >>
 use Bank;
 if(Bank[UserID + "-slimit"]) {
-  resp = "You must wait **1 minute** before using this command again!";
+  resp = "You must wait **10 seconds** before using this command again!";
 } else {
   if(Bank[UserID]) {
     if(Bank[UserID] >= 1000) {
       Bank[UserID + "-slimit"] = "active";
       Bank[UserID] = JSON.parse(Bank[UserID]) - 1000;
-      var slot = [":cherries:", ":snowflake:", ":four_leaf_clover:", ":star2:"];
+      var slot = [":cherries:", ":snowflake:", ":four_leaf_clover:", ":star2:", ":sunflower:", ":cyclone:", ":hearts:", ":mushroom:", ":six:", ":two:", ":cookie:"];
       var rand1 = Math.floor(Math.random() * slot.length);
       var rand2 = Math.floor(Math.random() * slot.length);
       var rand3 = Math.floor(Math.random() * slot.length);
@@ -25,21 +25,17 @@ if(Bank[UserID + "-slimit"]) {
       var a5 = slot[rand5];
       var a6 = slot[rand6];
       var a7 = slot[rand7];
-      var a8 = slot[rand8];
-      var a9 = slot[rand9];
-      if(a1 == a2) {
-        if(a2 == a3) {
-          Bank[UserID] = JSON.parse(Bank[UserID]) + 10000;
-          resp = "-.|" + a4 + "|" + a5 + "|" + a6 + "|\n>|" + a1 + "|" + a2 + "|" + a3 + "|<\n-.|" + a7 + "|" + a8 + "|" + a9 + "|\n\nYou won! You have gained **+10,000** credits!";
-        } else {
-          resp = "-.|" + a4 + "|" + a5 + "|" + a6 + "|\n>|" + a1 + "|" + a2 + "|" + a3 + "|<\n-.|" + a7 + "|" + a8 + "|" + a9 + "|\n\nBetter luck next time!";
-        }
+      var a8 = slot[rand6];
+      var a9 = slot[rand7];
+      if(a1 == a2 && a2 == a3) {
+        Bank[UserID] = JSON.parse(Bank[UserID]) + 10000;
+        resp = "-.|" + a4 + "|" + a5 + "|" + a6 + "|\n>|" + a1 + "|" + a2 + "|" + a3 + "|<\n-.|" + a7 + "|" + a8 + "|" + a9 + "|\n\nYou won! You have gained **+10,000** credits!";
       } else {
         resp = "-.|" + a4 + "|" + a5 + "|" + a6 + "|\n>|" + a1 + "|" + a2 + "|" + a3 + "|<\n-.|" + a7 + "|" + a8 + "|" + a9 + "|\n\nBetter luck next time!";
       }
-    } else {
-      resp = "You must have at least **1000** credits!";
     }
+  } else {
+    resp = "You must have at least **1000** credits!";
   } else {
     resp = "You need to have a Bank Account first! You can create one by typing **!bank register**";
   }

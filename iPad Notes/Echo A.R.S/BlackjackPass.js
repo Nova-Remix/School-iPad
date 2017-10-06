271eda1f210b9ba53986b7db624a2889e05053cb
Nova <robloxmuffinzcafe@gmail.com>
2017-10-06 13:33:03 -0400

    Update BlackjackPass.js

--- iPad Notes/Echo A.R.S/BlackjackPass.js
+++ iPad Notes/Echo A.R.S/BlackjackPass.js
@@ -5,6 +5,7 @@ use BlackJ;
 if(!BlackJ[UserID]) {
   resp = "You cannot use this command until you start a Blackjack game, <@" + UserID + ">! You can start one by typing **!blackjack**";
 } else {
+  var bank = JSON.parse(Bank[UserID]);
   var mycards = JSON.parse(BlackJ[UserID]);
   var dealer = JSON.parse(BlackJ[UserID + "-dealer"]);
   var bet = JSON.parse(BlackJ[UserID + "-bet"]);
@@ -22,4 +23,13 @@ if(!BlackJ[UserID]) {
       resp = "You won, <@" + UserID + ">! Your bet has been multiplied by 2 and added to your account!";
     } else {
       if(dealer == mycards) {
-        
+        Bank["UserID"] = bank + bet;
+        delete BlackJ[UserID];
+        delete BlackJ[UserID + "-bet"];
+        delete BlackJ[UserID + "-dealer"];
+        resp = "You and the dealer have the same amount, <@" + UserID + ">! Your bet has been given back to you!";
+      }
+    }
+  }
+}
+>>

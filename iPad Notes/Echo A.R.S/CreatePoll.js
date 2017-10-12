d5bff8fa7d186054fcb0c7546a536a3b6212937f
Nova <robloxmuffinzcafe@gmail.com>
2017-10-12 11:28:21 -0400

    Update CreatePoll.js

--- iPad Notes/Echo A.R.S/CreatePoll.js
+++ iPad Notes/Echo A.R.S/CreatePoll.js
@@ -1,1 +1,51 @@
-
+.auto &!poll {params}={init}
+#js >>
+use Poll;
+if(Params) {
+  var p = Params.split(";");
+  if(p[2] != null) {
+    var psliced = Params.slice(p[0].length).split(";");
+    resp = "__**" + p[0] + "**__\n";
+    for (var i = 1; i < psliced.length; i++) {
+      resp += "***" + i + ".*** *" + p[i] + "*\n";
+    }
+    if(psliced.length == 3) {
+      resp += "{reactbot::one: :two:}";
+    } else {
+      if(psliced.length == 4) {
+        resp += "{reactbot::one: :two: :three:}";
+      } else {
+        if(psliced.length == 5) {
+          resp += "{reactbot::one: :two: :three: :four:}";
+        } else {
+          if(psliced.length == 6) {
+            resp += "{reactbot::one: :two: :three: :four: :five:}";
+          } else {
+            if(psliced.length == 7) {
+              resp += "{reactbot::one: :two: :three: :four: :five: :six:}";
+            } else {
+              if(psliced.length == 8) {
+                resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven:}";
+              } else {
+                if(psliced.length == 9) {
+                  resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight:}";
+                } else {
+                  if(psliced.length == 10) {
+                    resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight: :nine:}";
+                  } else {
+                    resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight: :nine: :ten:}";
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  } else {
+    resp = "You must have at least 2 options to choose from, {user}!";
+  }
+} else {
+  resp = "You must supply the title and 2 or more options! **Example**\n\n!poll TITLE;OPTION1;OPTION2";
+}
+>>

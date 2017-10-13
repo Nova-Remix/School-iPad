cd1c48ff30227c5277317d819d7f0d6c86b340e4
Nova <robloxmuffinzcafe@gmail.com>
2017-10-13 09:45:43 -0400

    Update CreatePoll.js

--- iPad Notes/Echo A.R.S/CreatePoll.js
+++ iPad Notes/Echo A.R.S/CreatePoll.js
@@ -1,108 +1,27 @@
 .auto &!poll {params}={init}
-
 #js >>
-
-use Poll;
-
-if(Params) {
-
+if(Params.length > 0) {
   var p = Params.split(";");
-
   if(p[2] != null) {
-
-    var psliced = Params.slice(p[0].length).split(";");
-
-    resp = "__**" + p[0] + "**__\n";
-
-    for (var i = 1; i < psliced.length; i++) {
-
-      resp += "***" + i + ".*** *" + p[i] + "*\n";
-
-    }
-
-    if(psliced.length == 3) {
-
-      resp += "{reactbot::one: :two:}";
-
+    if(.slice(p[0].length).indexOf(" ") > -1) {
+      resp = "Please do not add spaces after `;`! **Example**\n\n!poll TITLE;OPTION1;OPTION2;ETC";
     } else {
-
-      if(psliced.length == 4) {
-
-        resp += "{reactbot::one: :two: :three:}";
-
-      } else {
-
-        if(psliced.length == 5) {
-
-          resp += "{reactbot::one: :two: :three: :four:}";
-
-        } else {
-
-          if(psliced.length == 6) {
-
-            resp += "{reactbot::one: :two: :three: :four: :five:}";
-
-          } else {
-
-            if(psliced.length == 7) {
-
-              resp += "{reactbot::one: :two: :three: :four: :five: :six:}";
-
-            } else {
-
-              if(psliced.length == 8) {
-
-                resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven:}";
-
-              } else {
-
-                if(psliced.length == 9) {
-
-                  resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight:}";
-
-                } else {
-
-                  if(psliced.length == 10) {
-
-                    resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight: :nine:}";
-
-                  } else {
-                    
-                    if(psliced.length == 11) {
-
-                    resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight: :nine: :keycap_ten:}";
-                      
-                    } else {
-                      
-                      resp += "{reactbot::one: :two: :three: :four: :five: :six: :seven: :eight: :nine: :keycap_ten: :pause_button:}";
-                      
-                    }
-
-                  }
-
-                }
-
-              }
-
-            }
-
-          }
-
-        }
-
+      var psliced = Params.slice(p[0].length).split(";");
+      var names = [":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":keycap_ten:", ":pause_button:"];
+      var react = "{reactbot:";
+      resp = "__**" + p[0] + "**__\n";
+      for(var i = 1; i < psliced.length; i++) {
+        resp += "***" + i + ".*** *" + p[i] + "*\n";
+        react += names[i] + " ";
       }
-
+      react = react.replace(/\s$/g, "");
+      react += "}";
+      resp += react;
+    } else {
+      resp = "You must have at least 2 options to choose from, {user}!";
     }
-
   } else {
-
-    resp = "You must have at least 2 options to choose from, {user}!";
-
+    resp = "You must supply the title and 2 or more options! **Example**\n\n!poll TITLE;OPTION1;OPTION2;ETC\n\nCurrently supports **11** options.";
   }
-
-} else {
-
-  resp = "You must supply the title and 2 or more options! **Example**\n\n!poll TITLE;OPTION1;OPTION2;ETC\n\nCurrently supports **11** options.";
-
 }
 >>

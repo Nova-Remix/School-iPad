982abfac5c6403f360d2c519eaf5475557f31880
Nova <robloxmuffinzcafe@gmail.com>
2017-11-28 10:13:37 -0500

    Delete Tentacle Photos in Array.js

--- Tentacle Photos in Array.js
+++
@@ -1,6 +0,0 @@
-[
-  "http://washington.carpediem.cd/data/afisha/o/e8/64/e86450b2ac.jpg",
-  "http://www.9gag2.com/wp-content/uploads/2017/05/hent4.jpg",
-  "http://i0.wp.com/www.bondara.co.uk/blog/wp-content/uploads/2013/09/hentai-tentacle-sex.jpg",
-  "https://i.pinimg.com/736x/bf/cf/55/bfcf5528f99ae74e2b01af60f7b412c8--anime-hot-tentacle.jpg"
-]
